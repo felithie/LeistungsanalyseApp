2cbfd0497f9da2e44901f0b5f380449dbd6e6c2d
felithie <felicitasthierbach@gmail.com>
2024-04-22 21:37:17 +0200

    6.2

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import my_classes as mc
 if __name__ == "__main__":
   sex= input ("Geben sie Ihr Geschlecht an (m/f):")
   first_name= input("Geben sie Ihren Vornamen an:")
-  last_name= input ("Geben sie ihren Nachnamen an:")
-  age = input ("Geben sie Ihr alter in Jahren an:")
-  age_years = int(age)
-  print(type(age_years))
-  print(age_years)
-  person1= mc.Person(first_name, last_name, sex, age_years)
-  mc.Person.save(person1, "person.json")
+  last_name= input("Geben sie ihren Nachnamen an:")
+  birthdate = input("Geben sie Ihr Geburtsdatum an:")
+
+  subject= mc.Subject(first_name, last_name, sex, birthdate)
+  mc.Subject.save(subject, "subject.json")
+
+  supervisor_vorname = input("Geben sie den Vornamen des Betreuers an:")
+  supervisor_nachname = input("Geben sie den Nachnamen des Betreuers an:")
+
+  supervisor= mc.Supervisor(supervisor_vorname, supervisor_nachname)
+  mc.Supervisor.save(supervisor, "supervisor.json")
 
   experiment_name = input("Geben sie den Namen des Experiments an:")
-  supervisor = input("Geben sie den Namen des Superivisors an:")
   date = input( "Geben sie das Datum von heute an:")
-  subject = input("Geben sie den Namen des Teilnehmers an:")
 
-  experiment1= mc.Experiment(experiment_name, date, supervisor, subject)
+  experiment1= mc.Experiment(experiment_name, date, supervisor.__dict__, subject.__dict__)
   mc.Experiment.save(experiment1, "experiment1.json")
--- my_classes.py
+++ my_classes.py
@@ -1,13 +1,30 @@
+from datetime import datetime
 import json
 
 class Person():
-    def __init__ (self, first_name, last_name, sex, age_years):
+    def __init__ (self, first_name, last_name):
         self.first_name= first_name
         self.last_name= last_name
-        self.sex = sex
-        self.age_years= int(age_years)
-        self.max_hr_bpm = self.estimate_max_hr()
 
+    def save(self,filename):
+        data= self.__dict__
+        with open (filename, "w") as file:
+            json.dump(data,file)
+
+class Subject(Person):
+    def __init__(self, first_name,last_name, sex, birthdate):
+        super().__init__(first_name, last_name)
+        self.__birthdate__ = birthdate
+        self.sex = sex
+        self.age_years = self.calculate_age()
+        self.max_hr = self.estimate_max_hr()
+    
+    def calculate_age(self):
+        today = datetime.today()
+        birthdate = datetime.strptime(self.__birthdate__, "%d.%m.%Y").date()
+        age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+        return int(age)
+    
     def estimate_max_hr(self):
         if self.sex =="m":
             max_hr_bpm = 223 -0.9 * self.age_years
@@ -16,11 +33,10 @@ class Person():
         else:
             max_hr_bpm = input("Gib deine maxHr ein:")
         return int(max_hr_bpm)
-    
-    def save(self,filename):
-        data= self.__dict__
-        with open (filename, "w") as file:
-            json.dump(data,file)
+
+class Supervisor(Person):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
 
 class Experiment():
     def __init__ (self,experiment_name, date, supervisor, subject):
