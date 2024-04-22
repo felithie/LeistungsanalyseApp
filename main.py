bf76b86525f6204256da290c25b297d1078b07bf
felithie <felicitasthierbach@gmail.com>
2024-04-22 21:08:48 +0200

    finished

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 import my_functions
-import my_classes
-
-sex= input ("Geben sie Ihr Geschlecht an (m/f):")
-first_name= input("Geben sie Ihren Vornamen an:")
-last_name= input ("Geben sie ihren Nachnamen an:")
-age = input ("Geben sie Ihr alter in Jahren an:")
-age_years = int(age)
-print(type(age_years))
-print(age_years)
-
-#if __name__=="__main__":
-    #estimateHR = my_functions.estimate_max_hr(age_years, sex)
-
+import my_classes as mc
 
 if __name__ == "__main__":
-  person1= Person (self, first_name, last_name, sex, age_years, __dict__) 
+  sex= input ("Geben sie Ihr Geschlecht an (m/f):")
+  first_name= input("Geben sie Ihren Vornamen an:")
+  last_name= input ("Geben sie ihren Nachnamen an:")
+  age = input ("Geben sie Ihr alter in Jahren an:")
+  age_years = int(age)
+  print(type(age_years))
+  print(age_years)
+  person1= mc.Person(first_name, last_name, sex, age_years)
+  mc.Person.save(person1, "person.json")
 
-    #person= my_functions.build_person(first_name, last_name, sex, age_years)
-    #print(person)
+  experiment_name = input("Geben sie den Namen des Experiments an:")
+  supervisor = input("Geben sie den Namen des Superivisors an:")
+  date = input( "Geben sie das Datum von heute an:")
+  subject = input("Geben sie den Namen des Teilnehmers an:")
 
-experiment_name = input("Geben sie den Namen des Experiments an:")
-supervisor = input("Geben sie den Namen des Superivisors an:")
-date = input( "Geben sie das Datum von heute an:")
-#subject = input("Geben sie den Namen des Teilnehmers an:")
-
-if __name__ == "__main__":
-  experiment1= Experiment (self,experiment_name, date, supervisor, subject, __dict1__)
-    #experiment= my_functions.build_experiment(experiment_name, date, supervisor, person)
-    #print(experiment)
+  experiment1= mc.Experiment(experiment_name, date, supervisor, subject)
+  mc.Experiment.save(experiment1, "experiment1.json")
--- my_classes.py
+++ my_classes.py
@@ -1,16 +1,18 @@
+import json
+
 class Person():
-    def __int__ (self, first_name, last_name, sex, age_years, __dict__):
+    def __init__ (self, first_name, last_name, sex, age_years):
         self.first_name= first_name
         self.last_name= last_name
         self.sex = sex
-        self.age_years= age_years
-        self.__dict__= dict
+        self.age_years= int(age_years)
+        self.max_hr_bpm = self.estimate_max_hr()
 
     def estimate_max_hr(self):
         if self.sex =="m":
-            max_hr_bpm = 223 -0.9 * self.age
+            max_hr_bpm = 223 -0.9 * self.age_years
         elif self.sex == "f":
-            max_hr_bpm = 226 -1.0 *self.age
+            max_hr_bpm = 226 -1.0 * self.age_years
         else:
             max_hr_bpm = input("Gib deine maxHr ein:")
         return int(max_hr_bpm)
@@ -21,12 +23,11 @@ class Person():
             json.dump(data,file)
 
 class Experiment():
-    def __int__ (self,experiment_name, date, supervisor, subject, __dict1__):
+    def __init__ (self,experiment_name, date, supervisor, subject):
         self.experiment_name = experiment_name
         self.date = date
         self.supervisor = supervisor
-        self.subjct = subject
-        self.__dict1__ = __dict1__
+        self.subject = subject
     
     def save(self,filename):
         data= self.__dict__
