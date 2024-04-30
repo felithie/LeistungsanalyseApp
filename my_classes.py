0e0bcdf5968567b0fed02d3de64f34e0ed07d7b8
felithie <felicitasthierbach@gmail.com>
2024-04-30 13:47:38 +0200

    attribut email und put person

--- my_classes.py
+++ my_classes.py
@@ -1,10 +1,19 @@
 from datetime import datetime
 import json
+import requests
+
+url = "http://127.0.0.1:5000/"
 
 class Person():
     def __init__ (self, first_name, last_name):
         self.first_name= first_name
         self.last_name= last_name
+    
+    def put (self):
+        data = {"name": self.first_name}
+        data_json = json.dumps(data)
+        response = requests.put(url, data=data_json)
+        print(response.text)
 
     def save(self,filename):
         data= self.__dict__
@@ -12,12 +21,15 @@ class Person():
             json.dump(data,file)
 
 class Subject(Person):
-    def __init__(self, first_name,last_name, sex, birthdate):
+    def __init__(self, first_name,last_name, sex, birthdate, email):
         super().__init__(first_name, last_name)
         self.__birthdate__ = birthdate
         self.sex = sex
         self.age_years = self.calculate_age()
         self.max_hr = self.estimate_max_hr()
+        self.email = email
+
+    
     
     def calculate_age(self):
         today = datetime.today()
